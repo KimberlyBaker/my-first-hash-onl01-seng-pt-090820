70681d2b62830b9969d68e577ebb96f5028dcf34
Kimberly Baker <bakerk2010@gmail.com>
2020-09-21 22:57:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,32 +4,18 @@ end
 
 
 def shipping_manifest
-  the_manifest = {
-    "whale bone corset" => 5,
-    "porcelain vase" => 2,
-    "oil painting" => 3
-  }
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  new_items = {
-  "muskets" => 2,
-  "gun powder" => 4
-}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   
-shipping_manifest
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 end
